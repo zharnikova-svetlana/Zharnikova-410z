e385cd8893dbb10b3c0f6afc89c858d09e4c020d
Светлана <lana.zharnikova@mail.ru>
2026-01-19 15:42:03 +0700

    Задание 2: Сериализация XML, JSON, и Binary завершена.

--- Task2Serialization/otus-sharp-4-main/otus-sharp-4-main/Otus.Task1/Models/GameSaveFile.cs
+++ Task2Serialization/otus-sharp-4-main/otus-sharp-4-main/Otus.Task1/Models/GameSaveFile.cs
@@ -1,113 +1,76 @@
 using System;
+using System.Xml.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Otus.Task1.Models
 {
-
-
-    public enum Gender{
+    public enum Gender
+    {
         None = 0,
-        Male=1,
-        Female=2,
+        Male = 1,
+        Female = 2,
     }
 
-    /// <summary>
-    /// Предмет
-    /// </summary>
+    [Serializable]
     public class Item
     {
-        /// <summary>
-        /// Название предмета
-        /// </summary>
-        /// <value></value>
         public string Name { get; set; }
 
-        /// <summary>
-        /// Количество
-        /// </summary>
-        /// <value></value>
-        public int Quantity { get; set; }
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (value < 0) throw new ArgumentException("Quantity cannot be less than 0");
+                _quantity = value;
+            }
+        }
     }
 
-    /// <summary>
-    /// Информация о пользователе
-    /// </summary>
+    [Serializable]
     public class User
     {
-        /// <summary>
-        /// Уровень пользователя
-        /// </summary>
-        /// <value></value>
+        [XmlAttribute("level")]
         public int Level { get; set; }
 
-        /// <summary>
-        /// Имя
-        /// </summary>
-        /// <value></value>
+        [JsonPropertyName("u")]
+        [XmlElement("u")]
         public string Name { get; set; }
 
+        [XmlIgnore]
+        [JsonIgnore]
+        public Gender Gender { get; set; }
 
-        /// <summary>
-        /// Пол персонажа
-        /// </summary>
-        /// <value></value>
-        public Gender Gender{get;set;}
+        [XmlElement("Gender")]
+        [JsonPropertyName("Gender")]
+        public string GenderString
+        {
+            get => Gender == Gender.Male ? "m" : Gender == Gender.Female ? "f" : "none";
+            set => Gender = value == "m" ? Gender.Male : value == "f" ? Gender.Female : Gender.None;
+        }
     }
 
-
-    /// <summary>
-    /// Состояние игры
-    /// </summary>
     [Serializable]
+    [XmlRoot("u")]
     public class GameStatus
     {
-
-
-        /// <summary>
-        /// Текущая локация
-        /// </summary>
-        /// <value></value>
         public string CurrentLocation { get; set; }
 
-        /// <summary>
-        /// Информация о пользователе
-        /// </summary>
-        /// <value></value>
         public User User { get; set; }
 
         public Item[] Items { get; set; }
 
-        /// <summary>
-        /// Координаты пользователя
-        /// </summary>
-        /// <value></value>
+        [XmlIgnore]
+        [JsonIgnore]
         public (double, double) Coords { get; set; }
     }
 
-    /// <summary>
-    /// Сохраняемый файл
-    /// </summary>
+    [Serializable]
     public class SaveFile : GameStatus
     {
-
-        /// <summary>
-        /// Дата создания файла
-        /// </summary>
-        /// <value></value>
         public DateTime CreatedDate { get; set; }
-
-        /// <summary>
-        /// Дата сохранения
-        /// </summary>
-        /// <value></value>
         public DateTime? SaveDate { get; set; }
-
-        /// <summary>
-        /// НАзвание файла
-        /// </summary>
-        /// <value></value>
         public string FileName { get; set; }
-
-
-
     }
 }
--- Task2Serialization/otus-sharp-4-main/otus-sharp-4-main/Otus.Task1/Program.cs
+++ Task2Serialization/otus-sharp-4-main/otus-sharp-4-main/Otus.Task1/Program.cs
@@ -1,34 +1,24 @@
 ﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using Otus.Task1.Models;
+
 namespace Otus.Task1
 {
     class Program
     {
-
-
         static SaveFile Generate1()
         {
             var res = new SaveFile();
             res.Coords = (1241.44, 124145.4);
             res.CurrentLocation = "Dungeon";
-            res.User = new User { Level = 10, Name = "Пушкин" };
+            res.User = new User { Level = 10, Name = "Пушкин", Gender = Gender.Male };
             res.Items = new[] { new Item() { Name = "Топор", Quantity = 2 } };
-
             return res;
-        }
-
-        static void SerializeBinary(SaveFile sf){
-
-        }
-
-        
-        static void SerializeJson(SaveFile sf){
-
-        }
-
-
-        static void SerializeXml(SaveFile sf){
-
         }
 
         static SaveFile Generate2()
@@ -36,25 +26,78 @@ namespace Otus.Task1
             var res = new SaveFile();
             res.Coords = (121.44, 124.4);
             res.CurrentLocation = "Subway";
-            res.User = new User { Level = 10, Name = "Feodorov" };
+            res.User = new User { Level = 10, Name = "Feodorov", Gender = Gender.Female };
             res.Items = new[] { new Item() { Name = "Stick", Quantity = -2 } };
-
             return res;
+        }
+
+        static void SerializeBinary(SaveFile sf)
+        {
+            try
+            {
+#pragma warning disable SYSLIB0011 
+                var formatter = new BinaryFormatter();
+                using (var stream = new FileStream("save.bin", FileMode.Create))
+                {
+                    formatter.Serialize(stream, sf);
+                }
+                Console.WriteLine("Binary serialization success.");
+            }
+            catch (Exception ex) { Console.WriteLine($"Binary Error: {ex.Message}"); }
+        }
+
+        static void SerializeJson(SaveFile sf)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                    WriteIndented = true
+                };
+
+                string jsonString = JsonSerializer.Serialize(sf, options);
+                File.WriteAllText("save.json", jsonString);
+                Console.WriteLine("JSON serialization success.");
+            }
+            catch (Exception ex) { Console.WriteLine($"JSON Error: {ex.Message}"); }
+        }
+
+        static void SerializeXml(SaveFile sf)
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(SaveFile));
+                using (var writer = new StreamWriter("save.xml"))
+                {
+                    xmlSerializer.Serialize(writer, sf);
+                }
+                Console.WriteLine("XML serialization success.");
+            }
+            catch (Exception ex) { Console.WriteLine($"XML Error: {ex.Message}"); }
         }
 
         static void Main(string[] args)
         {
             var g1 = Generate1();
-            var g2 = Generate2();
 
+            Console.WriteLine("--- Processing Object 1 ---");
             SerializeBinary(g1);
             SerializeJson(g1);
             SerializeXml(g1);
 
-            
-            SerializeBinary(g2);
-            SerializeJson(g2);
-            SerializeXml(g2);
+            Console.WriteLine("\n--- Processing Object 2 ---");
+            try
+            {
+                var g2 = Generate2();
+                SerializeBinary(g2);
+                SerializeJson(g2);
+                SerializeXml(g2);
+            }
+            catch (Exception ex) { Console.WriteLine($"Error: {ex.Message}"); }
+
+            Console.WriteLine("\nDone!");
+            Console.ReadLine();
         }
     }
 }
